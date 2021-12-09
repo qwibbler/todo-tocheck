b81aa24ce323ca6101f2c68d3b8feb32459a1c25
danielufeli <danielufeli@yahoo.com>
2021-12-09 16:28:47 +0100

    Add test for Remove Items and completed Items

--- src/add-remove.test.js
+++ src/add-remove.test.js
@@ -5,16 +5,16 @@ import documentToDo from './default-list.js';
 
 // const input = { value: 'Abc' }
 // const itemsEmpty = [];
-// const itemsFull = [{
-//   description: 'xyzzy',
-//   complete: false,
-//   index: 0,
-// },
-// {
-//   description: 'abcde',
-//   complete: true,
-//   index: 1,
-// }];
+const itemsFull = [{
+  description: 'xyzzy',
+  complete: false,
+  index: 0,
+},
+{
+  description: 'abcde',
+  complete: true,
+  index: 1,
+}];
 
 describe('Add...', () => {
   test('Abc to empty items', () => {
@@ -71,5 +71,62 @@ describe('Add...', () => {
     addRemove.addItem(input, itemsFull);
     documentToDo(itemsFull);
     expect(document.body.querySelectorAll('li').length).toBe(3);
+  })
+});
+
+describe('Remove...', () => {
+  test('Remove Item', () => {
+    const newArr = addRemove.removeItem(itemsFull[0], itemsFull);
+    expect(newArr.length).toBe(1);
+  })
+
+  test('Delete Item from DOM', () => {
+    document.body.innerHTML = `
+    <ul class="todo items">
+    </ul>
+    `
+    const itemsFull = [{
+      description: 'xyzzy',
+      complete: false,
+      index: 0,
+    },
+    {
+      description: 'abcde',
+      complete: true,
+      index: 1,
+    }];
+    const newArr = addRemove.removeItem(itemsFull[0], itemsFull);
+    documentToDo(newArr);
+    expect(document.body.querySelectorAll('li').length).toBe(1);
+  })
+
+  test('Delete All Completed Items', () => {
+    document.body.innerHTML = `
+    <ul class="todo items">
+    </ul>
+    `
+    const itemsFull = [{
+      description: 'xyzzy',
+      complete: false,
+      index: 0,
+    },
+    {
+      description: 'abcde',
+      complete: true,
+      index: 1,
+    },
+    {
+      description: 'Item 3',
+      complete: false,
+      index: 2,
+    },
+    {
+      description: 'Item 4',
+      complete: true,
+      index: 3,
+    }];
+    const newArr = addRemove.removeAllCompleted(itemsFull);
+    documentToDo(newArr);
+    expect(document.body.querySelectorAll('li').length).toBe(2);
   })
 });
